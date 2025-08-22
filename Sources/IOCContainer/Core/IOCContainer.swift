24a959a3d2647ad8bb6d6e2b1d67f2db4f7db890
Angel Dominguez Plata <angeldomp49@gmail.com>
2025-08-21 18:59:39 -0600

    ioc container

--- Sources/IOCContainer/Core/IOCContainer.swift
+++ Sources/IOCContainer/Core/IOCContainer.swift
@@ -2,24 +2,39 @@ import Foundation
 
 public class IOCContainer {
     private let beansLock = ReentrantReadWriteLock()
-    var beans: Set<BeanInformation> = []
+    var beansInternal: Set<BeanInformation> = []
+    
+    var beans: Set<BeanInformation>{
+        return beansLock.requestReadLock{
+            let unmodifiableBeans = Set(beansInternal)
+            return unmodifiableBeans
+        }
+    }
     
     private let singletonLock = ReentrantReadWriteLock()
-    let singletons: [String: Any] = [:]
+    var singletons: [String: Any] = [:]
     
     private let dependencyResolutionStrategy = BottomToTopResolutionStrategy()
     let context: EnvironmentContext
+    
+    private let dependencyRevisionLock = ReentrantReadWriteLock()
     let beanDependencyRevision = BeanDependencyRevision()
     
     public init(context: EnvironmentContext){
         self.context = context
     }
     
+    public funct getSi
+    
+    public func getSingleton(identifier: String) -> Any{
+        
+    }
+    
     public func registerAllEquivalents(equivalents: [BeanInformationEquivalent]){
         beansLock.requestWriteLock{
             let beans = equivalents.map{ bean in bean.toBeanInformation() }
             
-            self.beans = Set(beans)
+            self.beansInternal = Set(beans)
         }
     }
     
@@ -28,10 +43,18 @@ public class IOCContainer {
     }
     
     public func executeDependencyRevision(beanInformation: BeanInformation, consumer: () -> Void = {}){
-        beanDependencyRevision.
+        
+        dependencyRevisionLock.requestWriteLock{
+            beanDependencyRevision.activate(beanInformation: beanInformation)
+            consumer()
+            beanDependencyRevision.deActivate()
+        }
+        
     }
     
     public func addSingleton(identifier: String, instance: Any){
-        
+        singletonLock.requestWriteLock{
+            singletons[identifier] = instance
+        }
     }
 }
